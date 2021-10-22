6457de6663cfb568c11673d710a36b3f2bda9b94
mingyumkim93 <43638576+mingyumkim93@users.noreply.github.com>
2021-10-22 13:56:52 +0300

    updated side bar

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -4,6 +4,29 @@ import "./SideBar.css";
 
 function SideBar() {
   const { wishLists } = useContext(WishListsContext);
+  const approvedProductList = useMemo(() => {
+    let approvedProductList: { id: number; title: string; quantity: number; price: number }[] = [];
+    wishLists.forEach((wishList) => {
+      wishList.products.forEach((product) => {
+        const existingItem = approvedProductList.find((item) => item.id === product.productDetail.id);
+        if (existingItem) {
+          const index = approvedProductList.findIndex((item) => item.id === product.productDetail.id);
+          approvedProductList[index] = {
+            ...approvedProductList[index],
+            quantity: approvedProductList[index].quantity + product.approvedAmount
+          };
+        } else if (product.approvedAmount > 0)
+          approvedProductList.push({
+            id: product.productDetail.id,
+            title: product.productDetail.title,
+            quantity: product.approvedAmount,
+            price: product.productDetail.price
+          });
+      });
+    });
+    return approvedProductList;
+  }, [wishLists]);
+
   const totalSum = useMemo(() => {
     let totalSum = 0;
     wishLists.forEach((wishList) => {
@@ -16,14 +39,11 @@ function SideBar() {
 
   const totalDiscount = useMemo(() => {
     let totalDiscount = 0;
-    wishLists.forEach((wishList) => {
-      wishList.products.forEach((product) => {
-        if (product.approvedAmount > 1)
-          totalDiscount += trimNumber((product.productDetail.price / 10) * product.approvedAmount);
-      });
+    approvedProductList.forEach((product) => {
+      if (product.quantity > 1) totalDiscount += product.quantity * (product.price / 10);
     });
     return trimNumber(totalDiscount);
-  }, [wishLists]);
+  }, [approvedProductList]);
 
   const finalPrice = useMemo(() => trimNumber(totalSum - totalDiscount), [totalSum, totalDiscount]);
 
@@ -34,46 +54,41 @@ function SideBar() {
   return (
     <div>
       <div className="side-bar">
-        <ul>
-          {wishLists.map((wishList) => (
-            <li key={wishList.userId}>
-              <b>{wishList.userFirstName}</b>
-              <ol>
-                {wishList.products.map((product) => (
-                  <li key={product.productDetail.id}>
-                    {product.productDetail.title} X {product.approvedAmount}
-                    <div>
-                      €
-                      {product.approvedAmount > 1 ? (
-                        <span>
-                          <span className="before-discount">
-                            {trimNumber(product.productDetail.price * product.approvedAmount)}
-                          </span>
-                          <b>
-                            {trimNumber(
-                              product.productDetail.price * product.approvedAmount -
-                                (product.productDetail.price / 10) * product.approvedAmount
-                            )}
-                          </b>
-                        </span>
-                      ) : (
-                        <span>
-                          <b>{product.productDetail.price * product.approvedAmount}</b>
-                        </span>
-                      )}
-                    </div>
-                  </li>
-                ))}
-              </ol>
-            </li>
-          ))}
-        </ul>
-        <div>
-          <div>Total sum: {totalSum}</div>
-          <div>Discount: {totalDiscount}</div>
-          <div>Final price: {finalPrice}</div>
-          <button>Buy</button>
+        <div className="product-list">
+          <h4>List of products</h4>
+          <ol>
+            {approvedProductList.map((product) => (
+              <li key={product.id}>
+                {product.title} X {product.quantity}
+                <div>
+                  {product.quantity > 1 ? (
+                    <span>
+                      <span className="before-discount">{trimNumber(product.price * product.quantity)}</span>
+                      <b> {trimNumber(product.price * product.quantity - (product.price / 10) * product.quantity)}€</b>
+                    </span>
+                  ) : (
+                    <span>
+                      <b>{product.price * product.quantity}€</b>
+                    </span>
+                  )}
+                </div>
+              </li>
+            ))}
+          </ol>
         </div>
+
+        <ul>
+          <li>
+            <b>Total sum: {totalSum}€</b>
+          </li>
+          <li>
+            <b>Discount: {totalDiscount}€</b>
+          </li>
+          <li>
+            <b>Final price: {finalPrice}€</b>
+          </li>
+          <button className="button-buy">Buy</button>
+        </ul>
       </div>
     </div>
   );
