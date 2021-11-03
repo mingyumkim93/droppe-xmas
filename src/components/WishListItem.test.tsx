cfc5d667a3de53db63cda45532cc11ab684a70fc
mingyumkim93 <43638576+mingyumkim93@users.noreply.github.com>
2021-11-03 16:33:19 +0200

    wish-list transition test

--- src/components/WishListItem.test.tsx
+++ src/components/WishListItem.test.tsx
@@ -1,17 +1,18 @@
-import { render, screen } from "@testing-library/react";
+import { act, render, screen } from "@testing-library/react";
 import { fakeWishLists } from "__mock__";
 import WishListItem from "./WishListItem";
 
 describe("WishListItem", () => {
-  it("is open by default and closed when user clicks the list button", () => {
-    const wishList = fakeWishLists[0];
-    const { container } = render(<WishListItem wishList={wishList} />);
-    const listButton = screen.getByRole("button", { name: wishList.userFirstName });
-    const expandedList = container.getElementsByClassName("expanded-wish-list")[0];
-    expect(listButton).toBeInTheDocument();
-    expect(expandedList).toBeVisible();
-
-    listButton.click();
-    expect(expandedList).not.toBeVisible();
+  it("has grow-div height auto by default and the height become 0 when user clicks button", async () => {
+    await act(async () => {
+      const wishList = fakeWishLists[0];
+      const { container } = render(<WishListItem wishList={wishList} />);
+      const listButton = screen.getByRole("button", { name: new RegExp(wishList.userFirstName) });
+      expect(listButton).toBeInTheDocument();
+      const growDiv = container.getElementsByClassName("grow-div")[0];
+      expect(growDiv).toHaveStyle("height:auto");
+      await act(async () => listButton.click());
+      expect(growDiv).toHaveStyle("height:0px");
+    });
   });
 });
